a8f10d48448c6a27ecac54cb279e8083df3d99da
UsmanAhmad-11 <71057@gcslahore.edu.pk>
2023-02-14 23:34:29 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Program to add two numbers
+# Solution-1 (with pre-defined variables)
+
+# num1 = 30
+# num2 = 70
+# sum = num1+num2
+# print("the sum of given numbers is:",sum)
+
+# Solution-2 (with user-input)
+
+num1 = float(input("Enter a number here:"))
+num2 = float(input("Enter another number here"))
+sum = num1+num2
+print("The sum of the provided two numbers is:",sum)
